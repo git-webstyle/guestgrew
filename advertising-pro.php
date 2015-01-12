65af1a8e8828d37b0f4a027d185171147987eb63
georai <georgerailean@gmail.com>
2015-01-12 11:48:20 +0200

    guestgrew first

---
+++ advertising-pro.php
@@ -0,0 +1,72 @@
+<!-- LOAD HEADER -->
+
+<?php include("header.php"); ?>
+
+<!-- LOAD HEADER -->
+
+        <div class="breadcrumbs">
+
+            <div class="container">
+
+                <ol class="breadcrumb">
+
+                    <li><a href="#">Home</a></li>
+
+                    <li class="active">Terms of Service</li>
+
+                </ol>
+
+            </div>
+
+        </div>
+
+        <div class="midle_container advertising-pro">
+
+            <div class="intro-header">
+                <div class="container">
+
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="intro-message">
+                                <h1>advertizer pro</h1>
+                                <hr class="intro-divider">
+                                <h3>Get Advertiser Pro Account for $19.00!</h3>
+                                <a href="" class="btn-buy">Buy</a>
+                                <p>Note: Pro Advertiser is a Powerful Tool. We have launched it at a starting fees of $19 a month. Get it Now before the Price Rises.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /.container -->
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6">
+                      <div class="media-left">
+                        <img src="..." alt="...">
+                      </div>
+                      <div class="media-body">
+                        <p>Explorer member blogs - New blogs are added daily.</p>
+                      </div>
+                    </div>
+                    <div class="col-sm-6">
+                      <div class="media-left">
+                        <img src="..." alt="...">
+                      </div>
+                      <div class="media-body">
+                        <p>Explorer member blogs - New blogs are added daily.</p>
+                      </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+
+
+<!-- LOAD FOOTER -->
+
+<?php include("footer.php"); ?>
+
+<!-- LOAD FOOTER -->
