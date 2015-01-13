2281080a58ac70d6358a44032b27168bdbd00b93
georai <georgerailean@gmail.com>
2015-01-13 10:33:04 +0200

    Marti dimineata

---
+++ add_new_campaign.php
@@ -0,0 +1,140 @@
+<!-- LOAD HEADER -->
+<?php include("header.php"); ?>
+<!-- LOAD HEADER -->
+<div class="breadcrumbs">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li class="active">Add a new Social Campaign</li>
+        </ol>
+    </div>
+</div>
+<div class="midle_container">
+    <div class="container">
+        <div class="page">
+            <div class="page_title2">
+                <h2>Start your guest blogging campaign</h2>
+            </div>
+            <div class="page_block">
+                <form class="form-horizontal add_new_campaign">
+                    <div class="form-group">
+                        <label for="input1" class="col-sm-4 col-md-2 control-label">Your Capaign Name:</label>
+                        <div class="col-sm-8 col-md-10">
+                            <input type="text" class="form-control" id="input1">
+                            <span class="form_tip">e.g My Tehnology Campaign</span>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 half_inputs">
+                        <div class="row">
+                            <div class="col-md-6 half_input">
+                                <div class="row">
+                                    <div class="form-group">
+                                        <label for="select1" class="col-sm-4 control-label">Niche:</label>
+                                        <div class="col-sm-8"> 
+                                            <div class="select_clone">
+                                                <select class="form-control"  id="select1">
+                                                    <option>Select One</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                </select>                                               
+                                            </div>
+                                            <span class="form_tip">Select your campaign niche</span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="select2" class="col-sm-4 control-label">Backlinks:</label>
+                                        <div class="col-sm-8"> 
+                                            <div class="select_clone">
+                                                <select class="form-control"  id="select2">
+                                                    <option>Select One</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                </select>                                                
+                                            </div>
+                                            <span class="form_tip">Type of Backlink</span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="input2" class="col-sm-4 control-label">No of Articles:</label>
+                                        <div class="col-sm-8">
+                                            <input type="text" class="form-control" id="input2">
+                                            <span class="form_tip">e.g. 5</span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="input3" class="col-sm-4 control-label">Min Alexa Rank:</label>
+                                        <div class="col-sm-8">
+                                            <input type="text" class="form-control" id="input3">
+                                        </div>
+                                    </div>
+                                </div>                            
+                            </div>
+                            <div class="col-md-6 half_input">
+                                <div class="row">
+                                    <div class="form-group">
+                                        <label for="select3" class="col-sm-4 col-md-5 control-label">Campaign Type:</label>
+                                        <div class="col-sm-8 col-md-7"> 
+                                            <div class="select_clone">
+                                                <select class="form-control"  id="select3">
+                                                    <option>Guest Blogging</option>
+                                                    <option>2</option>
+                                                    <option>3</option>
+                                                    <option>4</option>
+                                                    <option>5</option>
+                                                </select>                                               
+                                            </div>
+                                            <span class="form_tip">Select your campaign type</span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="input4" class="col-sm-4 col-md-5 control-label">Blogger Budget:</label>
+                                        <div class="col-sm-8 col-md-7">
+                                            <input type="text" class="form-control" id="input4">
+                                            <span class="form_tip">Per Post Budget</span>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="input5" class="col-sm-4 col-md-5 control-label">Min Page Rank:</label>
+                                        <div class="col-sm-8 col-md-7">
+                                            <input type="text" class="form-control" id="input5">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="input6" class="col-sm-4 col-md-5 control-label">Domain Authority:</label>
+                                        <div class="col-sm-8 col-md-7">
+                                            <input type="text" class="form-control" id="input6">
+                                        </div>
+                                    </div>
+                                </div>                            
+                            </div>
+                        </div>                            
+                    </div>
+
+                    <div class="form-group">
+                        <label for="inputPassword3" class="col-sm-4 col-md-2 control-label">Publisher Notes</label>
+                        <div class="col-sm-8 col-md-10">
+                            <textarea  class="form-control" id="inputPassword3"></textarea>
+                            <span class="form_tip">e.g.: Only PR2+ DA 20+ blogs, strictly on niche</span>
+                        </div>
+                    </div>
+                    
+                    <div class="form-group">
+                        <div class="col-sm-offset-4 col-md-offset-2  col-sm-8 col-md-10">
+                            <button type="submit" class="gala4ka">Save campaign and next</button>
+                            
+                            <button type="button" class="btn btn-lg btn_grey btn_right">$0 Budget x 0 Articles = $0 USD</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- LOAD FOOTER -->
+<?php include("footer.php"); ?>
+<!-- LOAD FOOTER -->
