99caa0f0c829492d4b55647fee0ee87cdb971faa
Radu M <ip.radu@gmail.com>
2015-01-13 15:52:55 +0200

    Radu Comit

---
+++ board_message.php
@@ -0,0 +1,75 @@
+<!-- LOAD HEADER -->
+<?php include("header.php"); ?>
+<!-- LOAD HEADER -->
+<div class="breadcrumbs">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li class="active">Board Message</li>
+        </ol>
+    </div>
+</div>
+<div class="midle_container">
+    <div class="container">
+        <div class="page">
+            <div class="page_title2">
+                <h2>Board Message</h2>
+            </div>
+            <div class="page_block board_message">
+                <h3>Guest Crew</h3>
+                <p>You are either not logged in or do not have permission to view this page. <br>
+                    This could be because one of the following reasons:</p>
+                <ul class="blue_nrdots_ul">
+                    <li><span>1</span>You are not logged in or registered. Please use the form at the bottom of this page to login.</li>
+                    <li><span>2</span>You do not have permission to access this page. Are you trying to access administrative pages or a resource that you shouldn't be? 
+Check in the forum rules that you are allowed to perform this action.</li>
+                    <li><span>3</span>Your account may have been disabled by an administrator, or it may be awaiting account activation.</li>
+                    <li><span>4</span>You have accessed this page directly rather than using appropriate forms or links.</li>
+                </ul>
+                
+                <h3>Login</h3>
+                <form class="form-horizontal">
+                    
+                    <div class="col-xs-12 half_inputs">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="row">                                    
+                                    <div class="form-group">
+                                        <label for="input2" class="col-sm-2 col-md-4 control-label">Username:</label>
+                                        <div class="col-sm-10 col-md-8">
+                                            <input type="text" class="form-control" id="input2">                                           
+                                        </div>
+                                    </div>
+                                    
+                                </div>                            
+                            </div>
+                            <div class="col-md-6">
+                                <div class="row">
+                                    <div class="form-group">
+                                        <label for="input6" class="col-sm-2 col-md-4 control-label">Password:</label>
+                                        <div class="col-sm-10 col-md-8">
+                                            <input type="password" class="form-control" id="input6">
+                                        </div>
+                                    </div>
+                                </div>                            
+                            </div>
+                        </div>                            
+                    </div>
+                    
+                    <div class="form-group">
+                        <div class="col-sm-offset-2 col-md-offset-2  col-sm-10 col-md-10">
+                            <button type="submit" class="btn btn-md btn_blue">LOG IN</button>
+                            
+                            <a href="#" class="board_btm_link grey_link">Need to register? </a>
+                            <a href="#" class="board_btm_link grey_link">Forgotten your password?</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- LOAD FOOTER -->
+<?php include("footer.php"); ?>
+<!-- LOAD FOOTER -->
---
+++ contactUs.php
@@ -0,0 +1,80 @@
+<!-- LOAD HEADER -->
+<?php include("header.php"); ?>
+<!-- LOAD HEADER -->
+<div class="breadcrumbs">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li class="active">Contact Us</li>
+        </ol>
+    </div>
+</div>
+<div class="midle_container">
+    <div class="container">
+        <div class="page contactUs">
+            <div class="page_title2">
+                <h2>Contact Us</h2>
+                <span>Do you have any questions or suggestions? </span>
+            </div>
+            <div class="col-md-8"><div class="row">
+                    <div class="page_block margin_15_r">
+                        <h3>compose a message</h3>
+                        <form class="form-horizontal without_toltip">
+
+                            <div class="form-group">
+                                <label for="input1" class="col-sm-3 col-md-4 col-lg-4 control-label">Your Name:</label>
+                                <div class="col-sm-9 col-md-8 col-lg-8">
+                                    <input type="text" class="form-control" id="input1">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="input2" class="col-sm-3 col-md-4 col-lg-4 control-label">Your Email:</label>
+                                <div class="col-sm-9 col-md-8 col-lg-8">
+                                    <input type="text" class="form-control" id="input2">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="select1" class="col-sm-3 col-md-4 col-lg-4 control-label">Category:</label>
+                                <div class="col-sm-9 col-md-8 col-lg-8"> 
+                                    <div class="select_clone">
+                                        <select class="form-control"  id="select1">
+                                            <option>Select One</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>                                               
+                                    </div>
+
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <label for="inputPassword3" class="col-sm-3 col-md-4 col-lg-4  control-label">Message: </label>
+                                <div class="col-sm-9 col-md-8 col-lg-8">
+                                    <textarea  class="form-control" id="inputPassword3"></textarea>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div class="col-sm-offset-3 col-md-offset-4 col-sm-9 col-md-8">
+                                    <button type="submit" class="btn btn-md">Send message</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div></div>
+            <div class="col-md-4"><div class="row">
+                    <div class="page_block margin_15_l right_side">
+                        <h3 class="contact_left_title mail">Email</h3>
+                        <h3 class="contact_left_title skype">Skype</h3>
+                        <h3 class="contact_left_title social">Social Media</h3>
+                        <span class="social twitter2"><img src="images/twitter2.png"></span>
+                                <span class="social facebook2"><img src="images/facebook2.png"></span>
+                    </div>
+                </div></div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+<!-- LOAD FOOTER -->
+<?php include("footer.php"); ?>
+<!-- LOAD FOOTER -->
