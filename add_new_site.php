5efd410b9a6db3a5005fb22d4691cf5c2a0e82b4
georai <georgerailean@gmail.com>
2015-01-23 17:59:29 +0200

    Formulars 23-01-2015

---
+++ add_new_site.php
@@ -0,0 +1,241 @@
+<!-- LOAD HEADER -->
+<?php include("dash_header.php"); ?>
+<!-- LOAD HEADER -->
+<div class="breadcrumbs">
+    <div class="container">
+        <ol class="breadcrumb">
+            <li><a href="#">Home</a></li>
+            <li class="active">Dashboard</li>
+        </ol>
+    </div>
+</div>
+<div class="midle_container">
+    <div class="container">
+        <div class="page dashboard">
+            <div class="left_side_menu col-md-4" >
+                <div class="row" id="scroll-content">
+
+                    <div class="header_menu_mobile mobile_version">
+                        <button type="button" class="navbar-toggle close_navbar" data-toggle="dash" data-target="" data-canvas="body">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navmenu-brand" href="#">Guest Grew</a>
+                    </div>
+                    <div class="clone_elements"></div> <!-- do not remove this line -->
+                    <div class="dash_top_radio_links" >
+                        <a href="#" class="active" >Advertiser</a>
+                        <a href="#" class="" >Publisher</a>
+                        <div class="clear"></div>
+                    </div>
+                    <div class='dash_menu'>
+                        <div class="col-sm-4 cat_1">
+                            <div class="row">
+                                <ul class="cat_1_menu">
+                                    <li class="cat_1_item campaign" data-idtoopen="bl_1"><a href="#"><span></span>Campaigns</a></li>
+                                    <li class="cat_1_item offers" data-idtoopen="bl_2"><a href="#"><span></span>Offers</a></li>
+                                    <li class="cat_1_item message" data-idtoopen="bl_3"><a href="#"><span></span>Messages</a></li>
+                                    <li class="cat_1_item proadviser" data-idtoopen="bl_4"><a href="#"><span></span>ProAdvertiser</a></li>
+                                    <li class="cat_1_item settings" data-idtoopen="bl_5"><a href="#"><span></span>Settings</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-sm-8 cat_2">
+                            <div class="row">
+                                <div id="bl_1" class="cat_2_item_block">
+                                    <ul>
+                                        <li class="white"><a href="">Social Campaign<span></span></a></li>
+                                        <li><a href="">Live Compaigns<span>15</span></a></li>
+                                        <li><a href="">Complited Campaigns<span>15</span></a></li>
+                                        <li><a href="">Compaigns Drafts<span>15</span></a></li>
+                                        <li><a href="">Pending review from admin campaigns<span>15</span></a></li>
+                                        <li><a href="">Returned Campaigns<span>15</span></a></li>
+                                    </ul>
+                                </div>
+                                <div id="bl_2" class="cat_2_item_block">
+                                    <ul>
+                                        <li class="white"><a href="">Guest Bligging Offers<span></span></a></li>
+                                        <li><a href="">Pending Offers<span>3</span></a></li>
+                                        <li><a href="">Apllied Offers<span>23</span></a></li>
+                                        <li><a href="">Assigned Offers<span>15</span></a></li>
+                                        <li><a href="">Rejected Offers<span>1</span></a></li>
+                                        <li><a href="">Declined Offers<span>7</span></a></li>
+                                        <li><a href="">Completed Offers<span>52</span></a></li>
+                                        <li><a href="">Pending Offers<span>3</span></a></li>
+                                        <li><a href="">Apllied Offers<span>23</span></a></li>
+                                        <li><a href="">Assigned Offers<span>15</span></a></li>
+                                        <li><a href="">Rejected Offers<span>1</span></a></li>
+                                        <li><a href="">Declined Offers<span>7</span></a></li>
+                                        <li><a href="">Completed Offers<span>52</span></a></li>
+                                    </ul>
+                                </div>
+                                <div id="bl_3" class="cat_2_item_block">
+                                    <ul>
+<!--                                        <li class="white"><a href="">Guest Bligging Offers<span></span></a></li>-->
+                                        <li><a href="">Inbox<span>3</span></a></li>
+                                        <li><a href="">Sent Mail<span>23</span></a></li>
+                                        <li><a href="">Drafts<span>15</span></a></li>
+                                        <li><a href="">Important<span>1</span></a></li>
+                                        <li><a href="">Starred<span>7</span></a></li>   
+                                         
+                                    </ul>
+                                </div>
+                                <div id="bl_4" class="cat_2_item_block">
+                                    <ul>
+<!--                                        <li class="white"><a href="">Guest Bligging Offers<span></span></a></li>-->
+                                        <li><a href="">Inbox<span>3</span></a></li>
+                                        <li><a href="">Sent Mail<span>23</span></a></li>
+                                        <li><a href="">Drafts<span>15</span></a></li>
+                                        <li><a href="">Important<span>1</span></a></li>
+                                        <li><a href="">Starred<span>7</span></a></li>                                        
+                                    </ul>
+                                </div>
+                                <div id="bl_5" class="cat_2_item_block">
+                                    <ul>
+<!--                                        <li class="white"><a href="">Guest Bligging Offers<span></span></a></li>-->
+                                        <li><a href="">Inbox<span>3</span></a></li>
+                                        <li><a href="">Sent Mail<span>23</span></a></li>
+                                        <li><a href="">Drafts<span>15</span></a></li>
+                                        <li><a href="">Important<span>1</span></a></li>
+                                        <li><a href="">Starred<span>7</span></a></li>                                        
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="clear"></div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="right_side_content col-md-8">
+                <div class="row">
+                    <div class="right_top_buttons">
+                        <a href="#" class="btn_green"><i class="fa fa-caret-square-o-right fa-fw"></i>Start a Guest Blogging Campaign</a>
+                        <a href="#" class="btn_blue"><i class="fa fa-caret-square-o-right fa-fw"></i>Start a Social Campaign</a>                   
+                    </div>
+                    <div class="right_content">
+                        <div class="title-forms section-form">
+                            <h3>Add New Site</h3>
+                            <p>Please fill up the required field to submit your site.</p>
+                        </div>
+                        <form class="form-horizontal label-form">
+                            <div class="form-head-new-site section-form">
+                              <div class="form-group">
+                                <label for="inputEmail1" class="col-md-2 control-label">Site Name </label>
+                                <div class="col-md-10">
+                                  <input type="text" class="form-control" id="inputEmail1">
+                                  
+                                </div>
+                              </div>
+                              <div class="form-group">
+                                <label for="inputEmail2" class="col-md-2 control-label">Site URL *</label>
+                                <div class="col-md-10">
+                                  <input type="text" class="form-control" id="inputEmail2">
+                                </div>
+                              </div>
+                              <div class="form-group">
+                                <label for="inputEmail3" class="col-md-2 control-label">Previous Published Post on the site.</label>
+                                <div class="col-md-10">
+                                  <input type="text" class="form-control" id="inputEmail3">
+                                  <span class="form_tip">[Enter URL]</span>
+                                </div>
+                              </div>
+                              <div class="form-group">
+                                <label for="inputEmail4" class="col-md-2 control-label">Coins to Earn *</label>
+                                <div class="col-md-10">
+                                  <input type="text" class="form-control" id="inputEmail4">
+                                  <span class="form_tip">* For Normal Posts</span>
+                                </div>
+                              </div> 
+                              <div class="form-group">
+                                <label for="select1" class="col-md-2 control-label">Category *</label>
+                                <div class="col-md-4"> 
+                                    <div class="select_clone">
+                                        <select class="form-control" id="select1">
+                                            <option>General</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                        </select>                                               
+                                    </div>
+                                </div>
+                              </div>                                                                                         
+                            </div>
+                            <div class="title-forms section-form">
+                                <h3>Do you Accept Casino / Gambling related posts?</h3>
+                                <p>If Yes, then select Yes from the below dropdown. If your don't accept <br>Casino/Gambling related posts, then Leave it as No.</p>
+                            </div>
+                            <div class="section-form">
+                                  <div class="form-group">
+                                    <label for="select2" class="col-md-5 control-label">Accept Casino/Gambling/Pharma links *</label>
+                                    <div class="col-md-2"> 
+                                        <div class="select_clone">
+                                            <select class="form-control" id="select2">
+                                                <option>Yes</option>
+                                                <option>No</option>
+                                            </select>                                               
+                                        </div>
+                                    </div>
+                                    <label for="select2" class="col-md-2 control-label">Link Type *</label>
+                                    <div class="col-md-3"> 
+                                        <div class="select_clone">
+                                            <select class="form-control" id="select2">
+                                                <option>General</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </select>                                               
+                                        </div>
+                                        <span class="form_tip">* For Casino / Gambling Posts</span>
+                                    </div>                         
+                                </div>
+                                <div class="form-group">
+                                    <label for="inputEmail5" class="col-md-2 control-label">Coins to charge *</label>
+                                    <div class="col-md-10">
+                                        <input type="text" class="form-control" id="inputEmail5">
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-md-offset-2 col-md-7"> 
+                                        <label for=""><input type="checkbox" class="radio_input form-control">Display this site to our Premium Advertiser </label>
+                                        <div class="form_tip">* Keep this option checked if you want more offers...</div>
+                                    </div>                           
+                                    <div class="col-md-3 half_input">
+                                        <p class="btn-form">
+                                            <button class="btn_green btn_guest btn">Add new</button>
+                                        </p>                        
+                                    </div>
+                                </div>
+ 
+                                <div class="clear"></div>
+                            </div>
+                        </form> 
+                        <div class="clear"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="clear"></div>
+        </div>
+    </div>
+</div>
+<script>
+  $(document).ready(function() {
+    $('.radio_input').iCheck({
+        checkboxClass: 'icheckbox_square-blue',
+        radioClass: 'iradio_square-blue',
+        increaseArea: '20%' // optional
+    });
+    $('.checkbox_input').iCheck({
+        checkboxClass: 'icheckbox_minimal-blue',
+        radioClass: 'iradio_minimal-blue',
+        increaseArea: '20%' // optional
+    });   
+  }); 
+</script>
+<!-- LOAD FOOTER -->
+<?php include("footer.php"); ?>
+<!-- LOAD FOOTER -->
